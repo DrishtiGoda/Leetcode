534be44642be274e5ad15af7f1f1e71e5254cbc7
DrishtiGoda <goda.d@northeastern.edu>
2021-10-21 13:34:44 -0700

    updated queue code

--- src/Queue/Queue.java
+++ src/Queue/Queue.java
@@ -2,6 +2,7 @@ package Queue;
 
 public class Queue {
     public static void main(String[] args) {
-        System.out.printf("Basic code for queue");
+        System.out.println("Basic code for queue");
+        System.out.println("Queue updated");
     }
 }
