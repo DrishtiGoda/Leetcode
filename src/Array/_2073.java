32fc7325bd86490b599ced1c042eb1d5f383cc73
DrishtiGoda <goda.d@northeastern.edu>
2021-12-03 17:35:04 -0800

    Added 2073. Time Needed to Buy Tickets

--- src/Array/_2073.java
+++ src/Array/_2073.java
@@ -22,14 +22,14 @@ public class _2073 {
     public int timeRequiredToBuy(int[] tickets, int k) {
         int count = 0;
 
-        for (int i = 0; i<k; i++){
-            if(tickets[i] >= tickets[k])
+        for (int i = 0; i < k; i++) {
+            if (tickets[i] >= tickets[k])
                 count = count + tickets[k];
             else
                 count = count + tickets[i];
         }
-        for(int i = k+1; i<tickets.length; i++){
-            if(tickets[i] >= tickets[k]- 1)
+        for (int i = k + 1; i < tickets.length; i++) {
+            if (tickets[i] >= tickets[k] - 1)
                 count = count + tickets[k] - 1;
             else
                 count = count + tickets[i];
