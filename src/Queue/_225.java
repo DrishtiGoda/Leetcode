12f51719a40547b0389d1ac6cafd96a97f0da78f
DrishtiGoda <goda.d@northeastern.edu>
2021-12-26 13:09:00 -0500

    Added 225. Implement Stack using Queues

---
+++ src/Queue/_225.java
@@ -0,0 +1,55 @@
+/*mplement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+Implement the MyStack class:
+
+void push(int x) Pushes element x to the top of the stack.
+int pop() Removes the element on the top of the stack and returns it.
+int top() Returns the element on the top of the stack.
+boolean empty() Returns true if the stack is empty, false otherwise.
+
+Input
+["MyStack", "push", "push", "top", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 2, 2, false]
+
+Explanation
+MyStack myStack = new MyStack();
+myStack.push(1);
+myStack.push(2);
+myStack.top(); // return 2
+myStack.pop(); // return 2
+myStack.empty(); // return False
+*/
+
+
+package Queue;
+
+public class _225 {
+
+    Queue<Integer> queue;
+
+    public _225() {
+        this.queue = new LinkedList<Integer>();
+    }
+
+    public void push(int x) {
+        queue.add(x);
+        for(int i=0; i<queue.size()-1; i++)
+        {
+            queue.add(queue.poll());
+        }
+    }
+
+    public int pop() {
+        return queue.remove();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
