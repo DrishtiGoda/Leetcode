788bac741d44d1cb3a46dcf66feb596f07e09f8c
DrishtiGoda <goda.d@northeastern.edu>
2021-12-30 16:28:05 -0500

    Added 2114. Maximum Number of Words Found in Sentences

---
+++ src/Array/_2114.java
@@ -0,0 +1,29 @@
+/*A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+
+You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+
+Return the maximum number of words that appear in a single sentence.
+
+Input: sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
+Output: 6
+Explanation:
+- The first sentence, "alice and bob love leetcode", has 5 words in total.
+- The second sentence, "i think so too", has 4 words in total.
+- The third sentence, "this is great thanks very much", has 6 words in total.
+Thus, the maximum number of words in a single sentence comes from the third sentence, which has 6 words.
+
+*/
+
+package Array;
+
+public class _2114 {
+    public int mostWordsFound(String[] sentences) {
+        int count =0;
+        for(int i=0; i<sentences.length; i++){
+            String[] temp = sentences[i].split(" ");
+            if(count < temp.length)
+                count = temp.length;
+        }
+        return count;
+    }
+}
