40ec8343ec3a519511ab9e3a499645aaf7fbc1f5
DrishtiGoda <goda.d@northeastern.edu>
2022-01-02 18:46:39 -0500

    Added 2085. Count Common Words With One Occurrence

---
+++ src/Array/_2085.java
@@ -0,0 +1,35 @@
+/*
+Given two string arrays words1 and words2, return the number of strings that appear exactly once in each of the two arrays.
+
+* Input: words1 = ["leetcode","is","amazing","as","is"], words2 = ["amazing","leetcode","is"]
+Output: 2
+Explanation:
+- "leetcode" appears exactly once in each of the two arrays. We count this string.
+- "amazing" appears exactly once in each of the two arrays. We count this string.
+- "is" appears in each of the two arrays, but there are 2 occurrences of it in words1. We do not count this string.
+- "as" appears once in words1, but does not appear in words2. We do not count this string.
+Thus, there are 2 strings that appear exactly once in each of the two arrays.
+ */
+
+package Array;
+
+public class _2085 {
+    public int countWords(String[] words1, String[] words2) {
+        HashMap<String,Integer> map1 = new HashMap<>();
+        HashMap<String,Integer> map2 = new HashMap<>();
+
+        for(String word : words1){
+            map1.put(word,map1.getOrDefault(word,0)+1);
+        }
+
+        for(String word : words2){
+            map2.put(word, map2.getOrDefault(word,0) + 1);
+        }
+
+        int count = 0;
+        for (String word : words1)
+            if(map1.get(word) == 1 && map2.getOrDefault(word,0) == 1)
+                count ++;
+        return count;
+    }
+}
