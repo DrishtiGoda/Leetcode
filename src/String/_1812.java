b8fe40cb859c8dec5f704464f5ddf410eb3d7d9b
DrishtiGoda <goda.d@northeastern.edu>
2022-01-30 12:52:14 -0500

    Added 1812. Determine Color of a Chessboard Square

---
+++ src/String/_1812.java
@@ -0,0 +1,23 @@
+/*You are given coordinates, a string that represents the coordinates of a square of the chessboard. Below is a chessboard for your reference.
+Return true if the square is white, and false if the square is black.
+The coordinate will always represent a valid chessboard square. The coordinate will always have the letter first, and the number second.
+
+Input: coordinates = "a1"
+Output: false
+Explanation: From the chessboard above, the square with coordinates "a1" is black, so return false.
+*/
+
+
+package String;
+
+public class _1812 {
+    public boolean squareIsWhite(String coordinates) {
+
+        if((coordinates.charAt(0) - 'a'+1) % 2 != 0 && coordinates.charAt(1) % 2 != 0 ){
+            return false;
+        } else if ((coordinates.charAt(0) - 'a'+1) % 2 == 0 && coordinates.charAt(1) % 2 == 0 ){
+            return false;
+        }
+        return true;
+    }
+}
